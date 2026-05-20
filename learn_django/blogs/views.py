af671423d0298cd4d3df69b066dae731e977d187
Rupesh Rana <rupeshrana20274@gmail.com>
2026-05-20 18:18:22 +0530

    learn how to featch data from database(model)

--- learn_django/blogs/views.py
+++ learn_django/blogs/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Employees
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    employees = Employees.objects.all()
+    context = {
+        'employees':employees
+    }
+    return render(request,'home.html', context)
+
+def employee_details(reqest, pk):
+    try:
+        employee = Employees.objects.get(pk=pk)
+        context = {
+            'employee' : employee
+        }
+        return render(reqest, 'employee_details.html', context)
+        # return HttpResponse(employee) 
+    except : 
+        raise Http404
+    
